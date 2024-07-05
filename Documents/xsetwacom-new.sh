98bc8d3b821b282ead7df539bbec21b25629d1af
Christian Chávez <kevin.chavez@yachaytech.edu.ec>
2024-07-05 01:33:54 -0500

    added wacom config file

---
+++ Documents/xsetwacom-new.sh
@@ -0,0 +1,15 @@
+idspad=($(xsetwacom --list devices | grep "Wacom Intuos BT S Pad pad" | awk '{print $8}'))
+idsstylus=($(xsetwacom --list devices | grep "Wacom Intuos BT S Pen stylus" | awk '{print $8}'))
+
+for i in "${idspad[@]}"
+do
+        xsetwacom set $i MapToOutput HDMI-A-0
+        xsetwacom set $i Button 1 key +CTRL z
+        xsetwacom set $i Button 2 key +CTRL v
+        xsetwacom set $i Button 3 key 0xFF61
+done
+
+for i in "${idsstylus[@]}"
+do
+        xsetwacom set $i MapToOutput HDMI-A-0
+done
