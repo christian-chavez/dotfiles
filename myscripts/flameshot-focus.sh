9668bf29e40f35ed860a7196458f3f894af2f85d
Christian Chávez <kevin.chavez@yachaytech.edu.ec>
2025-06-18 12:43:02 -0500

    added file to solve focus-loss by flameshot

---
+++ myscripts/flameshot-focus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+focusedwindow=$(xdotool getactivewindow)
+flameshot gui  >/dev/null
+if [ "$focusedwindow" == "$(xdotool getactivewindow)" ]
+then
+	xdotool windowfocus $focusedwindow
+fi
