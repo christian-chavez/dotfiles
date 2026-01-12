d470c4e455ebbb5e50f412f04a802105423d359b
Christian Chávez <kevin.chavez@yachaytech.edu.ec>
2026-01-12 16:48:42 -0500

    updated rofi, now its like a sticky window

--- Documents/scripts/rofi-files.sh
+++ Documents/scripts/rofi-files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-file=$(find /mnt/Mis\ archivos/maitrise -type f -name '*.pdf' -print0 | while IFS= read -r -d '' path; do echo "$(basename "$path")                                                                                                                :::$path"; done | rofi -threads 0 -dmenu -i -p 'Files:' | awk -F ':::' '{print $2}') && [ -n "$file" ] && xdg-open "$file"
+file=$(find /mnt/Mis\ archivos/maitrise -type f -name '*.pdf' -print0 | while IFS= read -r -d '' path; do echo "$(basename "$path")                                                                                                                :::$path"; done | rofi -normal-window -threads 0 -dmenu -i -p 'Files:' | awk -F ':::' '{print $2}') && [ -n "$file" ] && xdg-open "$file"
 
 # file=$(find /mnt/Mis\ archivos/Google\ Drive/Libros -type f -name '*.pdf' -print0 | while IFS= read -r -d '' path; do echo "$(basename "$path")                                                                                                                :::$path"; done | rofi -threads 0 -dmenu -i -p 'Files:' | awk -F ':::' '{print $2}') && [ -n "$file" ] && xdg-open "$file"
