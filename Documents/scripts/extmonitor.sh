4e8d56e2a1f2bf4c14c741eaf2b7b938c4c3822e
Christian Chávez <christian.chavez@usherbrooke.ca>
2026-03-01 16:12:23 -0500

    added script to handle external monitor for bspwm

---
+++ Documents/scripts/extmonitor.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if xrandr -q | grep -q '^HDMI-A-0 connected'; then
+    if xrandr --verbose | grep -q 'HP E202'; then
+        # Special case for the old HP monitor
+        xrandr \
+            --output eDP --primary --mode 1366x768 --rotate normal \
+            --output HDMI-A-0 --mode 1600x900 --rate 60 --rotate normal --left-of eDP
+    else
+        # Generic behavior for other monitors/projectors
+        xrandr \
+            --output eDP --primary --mode 1366x768 --rotate normal \
+            --output HDMI-A-0 --auto --rotate normal --left-of eDP
+    fi
+
+    bspc wm -O HDMI-A-0 eDP
+    bspc monitor eDP -d 6 7 8 9 10
+    bspc monitor HDMI-A-0 -d 1 2 3 4 5
+else
+    xrandr --output HDMI-A-0 --off 2>/dev/null
+    bspc monitor eDP -d 1 2 3 4 5 6 7 8 9 10
+fi
