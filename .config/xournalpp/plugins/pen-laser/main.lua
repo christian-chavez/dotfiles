82352ba554f2bd5b85fdf6b981fd30c3bb6d5bd5
Christian Chávez <christian.chavez@usherbrooke.ca>
2026-03-15 12:20:13 -0400

    updated extended shortcuts (pen laser) plugin

--- .config/xournalpp/plugins/pen-laser/main.lua
+++ .config/xournalpp/plugins/pen-laser/main.lua
@@ -1,109 +1,42 @@
+local pensize = require('pen-size')
+local colorcycle = require('color-cycle')
+
 function initUi()
-    app.registerUi({
-        menu = 'Undo',
-        callback = 'doUndo',
-        accelerator = 'u'
-    })
+    registerShortcut('Undo', 'doUndo', 'g')
+    registerShortcut('Redo', 'doRedo', 'r')
+    registerShortcut('Cycle colors', 'cycleColors', 'f')
+    registerShortcut('Pen', 'usePen', 'c')
+    registerShortcut('Cycle pen size', 'cyclePenSize', '<Shift>c')
+    registerShortcut('Laser pointer', 'useLaser', 'x')
+    registerShortcut('Rectangle tool', 'useRectangle', 's')
+    registerShortcut('Arrow drawing', 'useArrow', 'w')
+    registerShortcut('Highlighter', 'useHighlighter', 'd')
+    registerShortcut('Text tool', 'useText', 't')
+    registerShortcut('Zoom fit', 'zoomFit', 'a')
+end
 
+function registerShortcut(menu, callback, accelerator)
     app.registerUi({
-        menu = 'Redo',
-        callback = 'doRedo',
-        accelerator = 'r'
-    })
-
-    app.registerUi({
-        menu = 'Cycle colors',
-        callback = 'cycleColors',
-        accelerator = 'f'
-    })
-
-    app.registerUi({
-        menu = 'Pen',
-        callback = 'usePen',
-        accelerator = 'c'
-    })
-
-    app.registerUi({
-        menu = 'Laser pointer',
-        callback = 'useLaser',
-        accelerator = 'x'
-    })
-
-    app.registerUi({
-        menu = 'Rectangle tool',
-        callback = 'useRectangle',
-        accelerator = 's'
-    })
-
-    app.registerUi({
-        menu = 'Arrow drawing',
-        callback = 'useArrow',
-        accelerator = 'w'
-    })
-
-    app.registerUi({
-        menu = 'Highlighter',
-        callback = 'useHighlighter',
-        accelerator = 'd'
-    })
-
-    app.registerUi({
-        menu = 'Text tool',
-        callback = 'useText',
-        accelerator = 't'
-    })
-
-    app.registerUi({
-        menu = 'Zoom fit',
-        callback = 'zoomFit',
-        accelerator = 'a'
+        menu = menu,
+        callback = callback,
+        accelerator = accelerator
     })
 end
 
-local colorList = {
-    { 'Scarlet', 0xFC2626 }, -- 252 38 38
-    { 'MrBlue',  0x0634F5 }, -- 6 52 245
-    { 'Darky',   0x241F31 }, -- 36 31 49
-}
+function doUndo()
+    app.uiAction({ action = 'ACTION_UNDO' })
+end
 
-local currentColor = 0
-
-function syncCurrentColorFromActiveTool()
-    local ok, toolInfo = pcall(function()
-        return app.getToolInfo('active')
-    end)
-
-    if not ok or toolInfo == nil or toolInfo['color'] == nil then
-        return
-    end
-
-    local activeColor = toolInfo['color']
-
-    for i = 1, #colorList do
-        if colorList[i][2] == activeColor then
-            currentColor = i
-            return
-        end
-    end
-
-    -- Active color is not one of your tracked colors.
-    -- So the next press of f will start again from the first color.
-    currentColor = 0
+function doRedo()
+    app.uiAction({ action = 'ACTION_REDO' })
 end
 
 function cycleColors()
-    syncCurrentColorFromActiveTool()
+    colorcycle.cycleColors()
+end
 
-    if currentColor < #colorList then
-        currentColor = currentColor + 1
-    else
-        currentColor = 1
-    end
-
-    app.changeToolColor({
-        color = colorList[currentColor][2],
-        selection = true
-    })
+function cyclePenSize()
+    pensize.cyclePenSize()
 end
 
 function clearShapeModes()
@@ -114,6 +47,7 @@ end
 function usePen()
     clearShapeModes()
     app.uiAction({ action = 'ACTION_TOOL_PEN' })
+    app.uiAction({ action = 'ACTION_SHAPE_RECOGNIZER', enabled = true })
 end
 
 function useLaser()
@@ -132,6 +66,7 @@ end
 function useHighlighter()
     clearShapeModes()
     app.uiAction({ action = 'ACTION_TOOL_HIGHLIGHTER' })
+    app.uiAction({ action = 'ACTION_SHAPE_RECOGNIZER', enabled = true })
 end
 
 function useText()
