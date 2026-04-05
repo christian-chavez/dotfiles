d55370e7f666a28032ce8174f7578f7bb2ac6c4c
Christian Chávez <christian.chavez@usherbrooke.ca>
2026-04-05 15:10:59 -0400

    updated battery notify script

--- .local/bin/alertsound.sh
+++ .local/bin/alertsound.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-mpv --no-config --no-video --really-quiet /home/christian/.local/bin/power-unplug.oga >/dev/null 2>&1 &
+paplay --stream-name='battery-alert' --volume=65536 "$HOME/.local/bin/power-low-louder.wav" >/dev/null 2>&1 &
+# paplay --stream-name='battery-alert' --volume=65536 "$HOME/.local/bin/power-low.oga" >/dev/null 2>&1 &
+
+
+# mpv --no-config --no-video --really-quiet /home/christian/.local/bin/power-unplug.oga >/dev/null 2>&1 &
 # mpv --no-config --no-video --really-quiet /home/christian/.local/bin/lowww.mp3 >/dev/null 2>&1 &
 # mpv --no-config --no-video --really-quiet /home/christian/.local/bin/alert.wav >/dev/null 2>&1 &
